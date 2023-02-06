5d9f97d306dd399044db377f42bd2fa42fadf58a
YoungPal <jygus513@gmail.com>
2023-02-06 11:17:30 +0900

    update editPort

--- SocketClient_v3_handler/app/src/main/java/com/kcci/socketclient/ClientThread.java
+++ SocketClient_v3_handler/app/src/main/java/com/kcci/socketclient/ClientThread.java
@@ -1,4 +1,5 @@
 package com.kcci.socketclient;
+
 import android.os.Bundle;
 import android.os.Message;
 import android.util.Log;
@@ -9,34 +10,31 @@ import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
-public class ClientThread extends Thread {
-    String serverIp;
-    int serverPort=5000;
+public class ClientThread extends Thread{
     Socket socket = null;
-    String clientId;
-    ClientThread(String strIp, String strId){
-        serverIp = strIp;
-        clientId = strId;
-    }
+
     @Override
     public void run() {
+
         try {
             socket = new Socket();
             displayText("[연결 요청]");
-            socket.connect(new InetSocketAddress(serverIp, serverPort));
+            socket.connect(new InetSocketAddress("10.10.141.2", 5000));
             displayText("[연결 성공]");
-            byte[] bytes = new byte[100];
             String message = null;
+            byte[] bytes = new byte[100];
+//            message = "hello client";
+//            sendData(message);
             InputStream is = socket.getInputStream();
-            while(true) {
+            while(true){
                 int readByteCount = is.read(bytes);
-                if(readByteCount <= 0)
+                if(readByteCount <=0)
                     break;
                 message = new String(bytes, 0, readByteCount, "UTF-8");
                 displayText("[데이터 받기 성공]: " + message);
-                sendMainActivity(message);
             }
             is.close();
+
         } catch (Exception e) {
             displayText("서버가 중지되었습니다");
         }
@@ -47,17 +45,9 @@ public class ClientThread extends Thread {
             } catch (IOException e1) {
             }
         }
+        displayText("클라이언트가 종료되었습니다");
     }
-    void stopClient(){
-        if(socket != null && !socket.isClosed()){
-            displayText("클라이언트 중지");
-            try {
-                socket.close();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
+
     synchronized void sendData(String data) { // final data
         Thread sendThread = new Thread() {
             @Override
@@ -75,14 +65,15 @@ public class ClientThread extends Thread {
         };
         sendThread.start();
     }
-    synchronized void sendMainActivity(String text) {
+    synchronized void sendMainActivity(String text){
         Message message = MainActivity.mainHandler.obtainMessage();
         Bundle bundle = new Bundle();
         bundle.putString("msg",text);
         message.setData(bundle);
         MainActivity.mainHandler.sendMessage(message);
     }
-    synchronized void displayText(String text) {
-        Log.d("displayText",text);
+    synchronized void displayText(String text){
+        Log.d("displayText", text);
+
     }
 }
--- SocketClient_v3_handler/app/src/main/java/com/kcci/socketclient/MainActivity.java
+++ SocketClient_v3_handler/app/src/main/java/com/kcci/socketclient/MainActivity.java
@@ -8,70 +8,39 @@ import android.os.Handler;
 import android.os.Message;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.ToggleButton;
-import android.widget.ScrollView;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 public class MainActivity extends AppCompatActivity {
     static Handler mainHandler;
     ClientThread clientThread;
     TextView textView;
-    ScrollView scrollView;
-    final SimpleDateFormat dataFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EditText editTextIp = findViewById(R.id.editTextIp);
-        EditText editTextId = findViewById(R.id.editTextId);
-        EditText editTextSend = findViewById(R.id.editTextSend);
-        ToggleButton toggleButton = findViewById(R.id.toggleButton);
-        Button buttonSend = findViewById(R.id.buttonSend);
-        textView = findViewById(R.id.textViewRec);
-        scrollView = findViewById(R.id.ScrollViewRecv);
-
-        toggleButton.setOnClickListener(new View.OnClickListener(){
+        Button button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(toggleButton.isChecked()){
-                    String strIp=editTextIp.getText().toString();
-                    String strId=editTextId.getText().toString();
-                    clientThread = new ClientThread(strIp, strId);
-                    clientThread.start();
-                    buttonSend.setEnabled(true);
-                } else{
-                    clientThread.stopClient();
-                    buttonSend.setEnabled(false);
+                clientThread = new ClientThread();
+                clientThread.start();
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
                 }
-
-            }
-        });
-        buttonSend.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String strSend = editTextSend.getText().toString();
-                clientThread.sendData(strSend);
-                editTextSend.setText("");
+                clientThread.sendData("Hello");
             }
         });
         mainHandler = new MainHandler();
     }
-    class MainHandler extends Handler {
+    class MainHandler extends Handler{
         @Override
-        public void handleMessage(@NonNull Message msg) {
+        public void handleMessage(@NonNull Message msg){
             super.handleMessage(msg);
-            Date date = new Date();
-            String strDate = dataFormat.format(date);
             Bundle bundle = msg.getData();
             String data = bundle.getString("msg");
-            strDate = strDate + " " + data+'\n';
-            textView.append(strDate);
-            scrollView.fullScroll(View.FOCUS_DOWN);
-
+            textView.append(data);
         }
     }
 }
